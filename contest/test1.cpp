182f198c271f5b40e2bfe35639ca292a17a1a1e9
khan3575 <sakibkhan.stud@gmail.com>
2025-10-17 15:37:50 +0600

    added code

---
+++ contest/test1.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+
+void solve()
+{
+	cout<<"hello\n";
+}
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	int t=1;
+	cin >> t;
+	while(t--){
+		solve();
+	}
+}
